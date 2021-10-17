f5e9fe399b1aea1aced7c88885515d621108dd9c
Hemory Phifer Jr <hemory@Hemorys-Mac-mini.local>
2021-10-16 22:13:05 -0400

    added date tiem prop to track when the note was created

--- NotesApp/Note.cs
+++ NotesApp/Note.cs
@@ -1,19 +1,23 @@
+using System;
+
 namespace NotesApp
 {
     public class Note
     {
         public string Message { get; set; }
         public string Title { get; set; }
+        public DateTime Date { get; set; }
 
         public Note(string message, string title)
         {
             Message = message;
             Title = title;
+            Date = DateTime.Now;
         }
 
         public override string ToString()
         {
-            return $"Title: {Title} Message: {Message}";
+            return $"Title: {Title} Message: {Message} Date Created: {Date.ToShortDateString()}";
         }
     }
 }
